aa25433b0ea0929a77f886a779214c0951678b81
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-07-24 14:50:34 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref} from "vue";
+import {onBeforeRender} from "lunchboxjs";
+
+const now = ref(Date.now() * 0.001)
+const earthRotation = ref({ y: 0 })
+const moonRotation = ref({ y: 0})
+
+onBeforeRender(() => {
+  now.value = Date.now() * 0.001
+  earthRotation.value.y = now.value * 2.8
+  moonRotation.value.y = now.value * 0.1
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Lunchbox :camera-position="[0, 0, 20]" :camera-look="[0, 0, 0]" shadow>
+    <directionalLight :intensity="1" :position="[0, 0, 20]"/>
+    <ambientLight :intensity="0.3"/>
+    <mesh :position-z="-10">
+      <planeGeometry :args="[50, 25]"/>
+      <meshPhysicalMaterial>
+        <textureLoader src="/2k_stars_milky_way.jpeg" attach="map"/>
+      </meshPhysicalMaterial>
+    </mesh>
+    <mesh :rotation-y="earthRotation.y">
+      <sphereGeometry :args="[1, 32, 32]"/>
+      <meshPhysicalMaterial>
+        <textureLoader src="/map-of-earth.avif" attach="map"/>
+        <textureLoader src="/grayscale-map-of-earth.avif" attach="bumpMap"/>
+      </meshPhysicalMaterial>
+    </mesh>
+    <mesh :position-z="Math.cos(now) * 10" :position-x="Math.sin(now) * 10" :scale="0.25" :rotation-y="moonRotation.y">
+      <sphereGeometry :args="[1, 32, 32]"/>
+      <meshPhysicalMaterial>
+        <textureLoader src="/moon-map.jpeg" attach="map"/>
+        <textureLoader src="/moon-bump-map.jpeg" attach="bumpMap"/>
+      </meshPhysicalMaterial>
+    </mesh>
+  </Lunchbox>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
